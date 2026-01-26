fcfe7086de479bbb42d492cb60f31dbd8a08825a
Bence Kovács <23636204+githappens@users.noreply.github.com>
2026-01-26 23:51:59 +0100

    Refactor GetVersionFromGitTag.cmake to streamline version detection logic and ensure proper handling of git tags and commits

--- GetVersionFromGitTag.cmake
+++ GetVersionFromGitTag.cmake
@@ -4,12 +4,7 @@
 # tags defined with semantic versioning 2.0.0 (http://semver.org/).
 #
 # The module expects the PROJECT_NAME variable to be set, and recognizes
-# the GIT_FOUND, GIT_EXECUTABLE and VERSION_UPDATE_FROM_GIT variables.
-# If Git is found and VERSION_UPDATE_FROM_GIT is set to boolean TRUE,
-# the project version will be updated using information fetched from the
-# most recent git tag and commit. Otherwise, the module will try to read
-# a VERSION file containing the full and partial versions. The module
-# will update this file each time the project version is updated.
+# the GIT_FOUND and GIT_EXECUTABLE variables. Git is required.
 #
 # Once done, this module will define the following variables:
 #
@@ -27,89 +22,67 @@
 # ${PROJECT_NAME}_VERSION_AHEAD - How many commits ahead of last tag (e.g. 21 in v2.3.1-RC.2+21.ef12c8)
 # ${PROJECT_NAME}_VERSION_GIT_SHA - The git sha1 of the most recent commit (e.g. the "ef12c8" in v2.3.1-RC.2+21.ef12c8)
 #
-# This module is public domain, use it as it fits you best.
-#
-# Author: Nuno Fachada
+# Based on work by Nuno Fachada (public domain)
 
-# Check if git is found...
-if (GIT_FOUND AND VERSION_UPDATE_FROM_GIT)
-
-	# Get last tag from git
-	execute_process(COMMAND ${GIT_EXECUTABLE} describe --abbrev=0 --tags
-		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-		OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_STRING
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-	#How many commits since last tag
-	execute_process(COMMAND ${GIT_EXECUTABLE} rev-list ${${PROJECT_NAME}_VERSION_STRING}..HEAD --count
-		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-		OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_AHEAD
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-	# Get current commit SHA from git
-	execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-		OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_GIT_SHA
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-	# Get partial versions into a list
-	string(REGEX MATCHALL "-.*$|[0-9]+" ${PROJECT_NAME}_PARTIAL_VERSION_LIST
-		${${PROJECT_NAME}_VERSION_STRING})
-
-	# Set the version numbers
-	list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
-		0 ${PROJECT_NAME}_VERSION_MAJOR)
-	list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
-		1 ${PROJECT_NAME}_VERSION_MINOR)
-	list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
-		2 ${PROJECT_NAME}_VERSION_PATCH)
-
-	# The tweak part is optional, so check if the list contains it
-	list(LENGTH ${PROJECT_NAME}_PARTIAL_VERSION_LIST
-		${PROJECT_NAME}_PARTIAL_VERSION_LIST_LEN)
-	if (${PROJECT_NAME}_PARTIAL_VERSION_LIST_LEN GREATER 3)
-		list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST 3 ${PROJECT_NAME}_VERSION_TWEAK)
-		string(SUBSTRING ${${PROJECT_NAME}_VERSION_TWEAK} 1 -1 ${PROJECT_NAME}_VERSION_TWEAK)
-	endif()
-
-	# Unset the list
-	unset(${PROJECT_NAME}_PARTIAL_VERSION_LIST)
-
-	# Set full project version string
-	set(${PROJECT_NAME}_VERSION_STRING_FULL
-		${${PROJECT_NAME}_VERSION_STRING}+${${PROJECT_NAME}_VERSION_AHEAD}.${${PROJECT_NAME}_VERSION_GIT_SHA})
-
-	# Save version to file (which will be used when Git is not available
-	# or VERSION_UPDATE_FROM_GIT is disabled)
-	file(WRITE ${CMAKE_SOURCE_DIR}/VERSION ${${PROJECT_NAME}_VERSION_STRING_FULL}
-		"*" ${${PROJECT_NAME}_VERSION_STRING}
-		"*" ${${PROJECT_NAME}_VERSION_MAJOR}
-		"*" ${${PROJECT_NAME}_VERSION_MINOR}
-		"*" ${${PROJECT_NAME}_VERSION_PATCH}
-		"*" ${${PROJECT_NAME}_VERSION_TWEAK}
-		"*" ${${PROJECT_NAME}_VERSION_AHEAD}
-		"*" ${${PROJECT_NAME}_VERSION_GIT_SHA})
-
-else()
-
-	# Git not available, get version from file
-	file(STRINGS ${CMAKE_SOURCE_DIR}/VERSION ${PROJECT_NAME}_VERSION_LIST)
-	string(REPLACE "*" ";" ${PROJECT_NAME}_VERSION_LIST ${${PROJECT_NAME}_VERSION_LIST})
-	# Set partial versions
-	list(GET ${PROJECT_NAME}_VERSION_LIST 0 ${PROJECT_NAME}_VERSION_STRING_FULL)
-	list(GET ${PROJECT_NAME}_VERSION_LIST 1 ${PROJECT_NAME}_VERSION_STRING)
-	list(GET ${PROJECT_NAME}_VERSION_LIST 2 ${PROJECT_NAME}_VERSION_MAJOR)
-	list(GET ${PROJECT_NAME}_VERSION_LIST 3 ${PROJECT_NAME}_VERSION_MINOR)
-	list(GET ${PROJECT_NAME}_VERSION_LIST 4 ${PROJECT_NAME}_VERSION_PATCH)
-	list(GET ${PROJECT_NAME}_VERSION_LIST 5 ${PROJECT_NAME}_VERSION_TWEAK)
-	list(GET ${PROJECT_NAME}_VERSION_LIST 6 ${PROJECT_NAME}_VERSION_AHEAD)
-	list(GET ${PROJECT_NAME}_VERSION_LIST 7 ${PROJECT_NAME}_VERSION_GIT_SHA)
-
+if (NOT GIT_FOUND)
+	message(FATAL_ERROR "Git is required for version detection")
 endif()
 
+# Get last tag from git
+execute_process(COMMAND ${GIT_EXECUTABLE} describe --abbrev=0 --tags
+	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_STRING
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+	RESULT_VARIABLE GIT_DESCRIBE_RESULT)
+
+if (NOT GIT_DESCRIBE_RESULT EQUAL 0)
+	message(FATAL_ERROR "No git tags found. Create an initial version tag (e.g., 'git tag v0.0.0')")
+endif()
+
+# How many commits since last tag
+execute_process(COMMAND ${GIT_EXECUTABLE} rev-list ${${PROJECT_NAME}_VERSION_STRING}..HEAD --count
+	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_AHEAD
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# Get current commit SHA from git
+execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+	OUTPUT_VARIABLE ${PROJECT_NAME}_VERSION_GIT_SHA
+	OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+# Get partial versions into a list
+string(REGEX MATCHALL "-.*$|[0-9]+" ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+	${${PROJECT_NAME}_VERSION_STRING})
+
+# Set the version numbers
+list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+	0 ${PROJECT_NAME}_VERSION_MAJOR)
+list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+	1 ${PROJECT_NAME}_VERSION_MINOR)
+list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+	2 ${PROJECT_NAME}_VERSION_PATCH)
+
+# The tweak part is optional, so check if the list contains it
+list(LENGTH ${PROJECT_NAME}_PARTIAL_VERSION_LIST
+	${PROJECT_NAME}_PARTIAL_VERSION_LIST_LEN)
+if (${PROJECT_NAME}_PARTIAL_VERSION_LIST_LEN GREATER 3)
+	list(GET ${PROJECT_NAME}_PARTIAL_VERSION_LIST 3 ${PROJECT_NAME}_VERSION_TWEAK)
+	string(SUBSTRING ${${PROJECT_NAME}_VERSION_TWEAK} 1 -1 ${PROJECT_NAME}_VERSION_TWEAK)
+endif()
+
+# Unset the list
+unset(${PROJECT_NAME}_PARTIAL_VERSION_LIST)
+
+# Set full project version string
+set(${PROJECT_NAME}_VERSION_STRING_FULL
+	${${PROJECT_NAME}_VERSION_STRING}+${${PROJECT_NAME}_VERSION_AHEAD}.${${PROJECT_NAME}_VERSION_GIT_SHA})
 
 # Set project version (without the preceding 'v')
 set(${PROJECT_NAME}_VERSION ${${PROJECT_NAME}_VERSION_MAJOR}.${${PROJECT_NAME}_VERSION_MINOR}.${${PROJECT_NAME}_VERSION_PATCH})
 if (${PROJECT_NAME}_VERSION_TWEAK)
 	set(${PROJECT_NAME}_VERSION ${${PROJECT_NAME}_VERSION}-${${PROJECT_NAME}_VERSION_TWEAK})
 endif()
+
+# Write VERSION file (used by build scripts)
+file(WRITE ${CMAKE_SOURCE_DIR}/VERSION "${${PROJECT_NAME}_VERSION}\n")
