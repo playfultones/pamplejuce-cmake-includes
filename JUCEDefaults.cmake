b7eac90c2bfebc9a41372be5b61d4d3459322fe7
Bence Kovács <23636204+githappens@users.noreply.github.com>
2026-01-27 00:28:25 +0100

    Enable generation of compile_commands.json for LSP/clangd support

--- JUCEDefaults.cmake
+++ JUCEDefaults.cmake
@@ -3,6 +3,9 @@
 # https://github.com/juce-framework/JUCE/commit/6b1b4cf7f6b1008db44411f2c8887d71a3348889
 set_property(GLOBAL PROPERTY USE_FOLDERS YES)
 
+# Generate compile_commands.json for LSP/clangd support
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
 # Creates a /Modules directory in the IDE with the JUCE Module code
 option(JUCE_ENABLE_MODULE_SOURCE_GROUPS "Show all module sources in IDE projects" ON)
 
